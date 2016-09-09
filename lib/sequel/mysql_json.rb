fa63bada2bed03500830d1f346a1ed17477f3362
Damián Silvani <munshkr@gmail.com>
2016-09-09 20:20:30 -0300

    Initial commit

---
+++ lib/sequel/mysql_json.rb
@@ -0,0 +1,7 @@
+require "sequel/mysql_json/version"
+
+module Sequel
+  module MysqlJson
+    # Your code goes here...
+  end
+end
---
+++ lib/sequel/mysql_json/version.rb
@@ -0,0 +1,5 @@
+module Sequel
+  module MysqlJson
+    VERSION = "0.1.0"
+  end
+end
