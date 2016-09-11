42a4aa2edf7034e67939ba58ca4478062f7c0392
Damián Silvani <munshkr@gmail.com>
2016-09-11 11:36:02 -0300

    Support most of the JSON built-in functions

--- lib/sequel/extensions/mysql_json_ops.rb
+++ lib/sequel/extensions/mysql_json_ops.rb
@@ -11,7 +11,52 @@ module Sequel
     class JSONOp < Sequel::SQL::Wrapper
       SPACE_RE = /\s+/
 
-      # Extract a value as json
+      # Append values to the end of the indicated arrays within a JSON document
+      # and return the result.
+      #
+      def array_append(*args)
+        json_op(:array_append, self, *path_value_args(args))
+      end
+
+      # Update a JSON document, inserting into an array within the document and
+      # returning the modified document.
+      #
+      def array_insert(*args)
+        json_op(:array_insert, self, *path_value_args(args))
+      end
+
+      # Return true or false whether a specific +value+ is contained in a
+      # target JSON document.
+      #
+      # If a +path+ argument is given, check at a specific path within the
+      # target document.
+      #
+      def contains(val, path=nil)
+        fn = function(:contains, self, Sequel.object_to_json(val), path)
+        Sequel::SQL::BooleanExpression.new(:NOOP, fn)
+      end
+
+      # Return whether a JSON document contains data at a given path or paths
+      #
+      # Returns false if no specified path exists within the document.
+      # Otherwise, the return value depends on the +one_or_all+ argument:
+      #
+      #  * one: true if at least one path exists within the document
+      #  * all: true if all paths exist within the document
+      #
+      def contains_path(one_or_all, *paths)
+        fn = json_op(:contains_path, self, one_or_all.to_s, *paths)
+        Sequel::SQL::BooleanExpression.new(:NOOP, fn)
+      end
+
+      # Return the maximum depth of a JSON document
+      #
+      def depth
+        Sequel::SQL::NumericExpression.new(:NOOP, function(:depth, self))
+      end
+
+      # Return data from a JSON document, selected from the parts of the
+      # document matched by the path arguments.
       #
       #   json_op.extract('[0]')   # JSON_EXTRACT(json, '$[0]')
       #   json_op.extract('.a')    # JSON_EXTRACT(json, '$.a')
@@ -37,36 +82,59 @@ module Sequel
       alias :[]  :extract
       alias :get :extract
 
-      # Replace values for paths that exist and add values
-      # for paths that do not exist.
-      #
-      def set(*args)
-        json_op(:set, self, *args)
-      end
-
-      # Add new values but does not replace existing values
+      # Insert data into a JSON document and return the result
       #
       def insert(*args)
-        json_op(:insert, self, *args)
+        json_op(:insert, self, *path_value_args(args))
       end
 
-      # Replace existing values and ignore new values
+      # Return the keys from the top-level value of a JSON object as a JSON array
       #
-      def replace(*args)
-        json_op(:replace, self, *args)
+      # If a +path+ argument is given, the top-level keys from the selected path. 
+      #
+      def keys(path=nil)
+        json_op(:keys, self, path)
+      end
+
+      # Return the length of JSON document
+      #
+      # If a +path+ argument is given, the length of the value within the
+      # document identified by the path.
+      #
+      def length(path=nil)
+        Sequel::SQL::NumericExpression.new(:NOOP, function(:length, self, path))
+      end
+
+      # Merges two or more JSON documents and returns the merged result
+      #
+      def merge(*docs)
+        json_op(:merge, self, *docs.map { |d| Sequel.object_to_json(d) })
       end
 
       # Take a JSON document and one or more paths that specify values
       # to be removed from the document.
       #
-      def remove(*args)
-        json_op(:remove, self, *args)
+      def remove(*paths)
+        json_op(:remove, self, *paths)
       end
 
-      # Take two or more JSON documents and return the combined result
+      # Replace existing values and ignore new values
       #
-      def merge(*args)
-        json_op(:merge, self, *args)
+      def replace(*args)
+        json_op(:replace, self, *path_value_args(args))
+      end
+
+      # Returns the path to the given string within a JSON document
+      #
+      def search(one_or_all, search_str, escape_char=nil, *paths)
+        json_op(:search, self, one_or_all.to_s, search_str, escape_char, *paths)
+      end
+
+      # Replace values for paths that exist and add values
+      # for paths that do not exist.
+      #
+      def set(*args)
+        json_op(:set, self, *path_value_args(args))
       end
 
       # Return the value's JSON type if it is valid and produces an error
@@ -109,6 +177,32 @@ module Sequel
       # Return quoted key if it has spaces
       def quote(key)
         key.to_s.index(SPACE_RE) ? "\"#{key}\"" : key
+      end
+
+      # Serialize literal values from a flattened (path, value) pairs array
+      def path_value_args(args)
+        args.each_slice(2)
+          .map { |p, v| [p, serialize_literal(v)] }.flatten(1)
+      end
+
+      # Serialize a Ruby value into a MySQL json literal
+      #
+      # Arrays and hashes are written as JSON_ARRAY and JSON_OBJECT function
+      # calls.
+      #
+      def serialize_literal(value)
+        case value
+        when NilClass, FalseClass, TrueClass, String, Numeric
+          value
+        when Symbol
+          value.to_s
+        when Array
+          function(:array, *value.map { |v| serialize_literal(v) })
+        when Hash
+          function(:object, *value.map { |k, v| [k.to_s, serialize_literal(v)] }.flatten(1))
+        else
+          Sequel.object_to_json(value)
+        end
       end
     end
 
